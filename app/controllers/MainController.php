0b8e90d0d0e0b5fc05b2a6ceeee9364e23e465c0
PopovDmytro <dimahom@ukr.net>
2018-04-18 20:49:48 +0300

    class Registry

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,24 +10,11 @@ class MainController extends AppController
 //    public $layout = 'main';
 
     function indexAction(){
-//        $this->layout = false;
-//        $this->layout = 'main';
-//        $this->view = 'test';
-
         $model = new Main;
         $posts = \R::findAll('posts');
         $post = \R::findOne('posts', 'id = 1');
-        debug($post);
-
+//        debug($post);
         $menu = $this->menu;
-//          $res = $model->query("CREATE TABLE posts2 SELECT * FROM fw_php.post");
-//          $posts = $model->findAll();
-//          $posts2 = $model->findAll();
-//          $post = $model->findOne(2);
-//          $data = $model->findBySql("SELECT * FROM post ORDER BY id DESC LIMIT 2");
-//          $data = $model->findBySql("SELECT * FROM {$model->table} WHERE name LIKE ?", ['%Tit%']);
-//          $data = $model->findLike('Tit', 'name');
-//          debug($data);
         $title = "PAGE TILE";
         $this->setMeta('Main Page', 'Description page', 'Key Words');
         $meta = $this->meta;
---
+++ public/test/classes/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace classes;
+
+
+class Test
+{
+
+    function go() {
+        echo "Test go go !!!";
+    }
+}
